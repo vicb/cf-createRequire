b016cb88a23526c3ae7afc12989413929ed69b4a
Victor Berchet <victor@suumit.com>
2024-09-04 22:46:34 +0200

    createRequire test

---
+++ README.md
@@ -0,0 +1,9 @@
+## Repro
+
+Point to a recent worked using `export MINIFLARE_WORKERD_PATH=/path/to/workerd`
+
+pnpm i
+
+pnpm dev
+
+press b - or curl <http://localhost:xxx>
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,14 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { createRequire } from 'node:module';
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		const esm = (await import('./hello.js')).hello();
+		console.log(esm);
+
+		const require = createRequire('file://');
+		const cjs = require('hello.cjs').hello();
+		console.log(cjs);
+
+		return new Response('done');
 	},
 } satisfies ExportedHandler<Env>;
